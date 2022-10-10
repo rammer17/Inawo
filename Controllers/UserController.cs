b9345ab747434428d2e8f1a3dd35cc4a38975d2b
Georgi <georgi.rosenov17@gmail.com>
2022-10-10 18:34:00 +0300

    email sender

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Net.Mail;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
@@ -25,9 +27,11 @@ namespace Inawo.Controllers
         }
 
         [HttpGet]
-        [Authorize]
+        //[Authorize]
         public ActionResult<List<UserGetResponse>> GetAll()
         {
+            SendEmail();
+
             var users = _dbContext.Users
                 .Include(x => x.Incomes)
                 .Include(x => x.Expenses)
@@ -252,6 +256,28 @@ namespace Inawo.Controllers
             {
                 throw new Exception("Id value is null");
             }
+        }
+
+        private void SendEmail()
+        {
+            string fromMail = "georgi.rosenov17@gmail.com";
+            string fromPassword = "hktfoylqqmsjrebe";
+
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(fromMail);
+            message.Subject = "Test Subject";
+            message.To.Add(new MailAddress("ninebet186@lutota.com"));
+            message.Body = "<html><head><style>#customers {font-family: Arial, Helvetica, sans-serif;border-collapse: collapse;width: 100%;}#customers td, #customers th {border: 1px solid #ddd;padding: 8px;}#customers tr:nth-child(even){background-color: #f2f2f2;}#customers tr:hover {background-color: #ddd;}#customers th {padding-top: 12px;padding-bottom: 12px;text-align: left;background-color: #04AA6D;color: white;}</style></head><body><h1>A Fancy Table</h1><table id=\"customers\"><tr><th>Company</th><th>Contact</th><th>Country</th></tr><tr><td>Alfreds Futterkiste</td><td>Maria Anders</td><td>Germany</td></tr><tr><td>Berglunds snabbköp</td><td>Christina Berglund</td><td>Sweden</td></tr><tr><td>Centro comercial Moctezuma</td><td>Francisco Chang</td><td>Mexico</td></tr><tr><td>Ernst Handel</td><td>Roland Mendel</td><td>Austria</td></tr><tr><td>Island Trading</td><td>Helen Bennett</td><td>UK</td></tr><tr><td>Königlich Essen</td><td>Philip Cramer</td><td>Germany</td></tr><tr><td>Laughing Bacchus Winecellars</td><td>Yoshi Tannamuri</td><td>Canada</td></tr><tr><td>Magazzini Alimentari Riuniti</td><td>Giovanni Rovelli</td><td>Italy</td></tr><tr><td>North/South</td><td>Simon Crowther</td><td>UK</td></tr><tr><td>Paris spécialités</td><td>Marie Bertrand</td><td>France</td></tr></table></body></html>";
+            message.IsBodyHtml = true;
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(fromMail, fromPassword),
+                EnableSsl = true
+            };
+
+            smtpClient.Send(message);
         }
 
     }
